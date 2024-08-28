2459b74e9800b08d89a74b22cf535e0f1428c3f4
Gabriel de Proença Lopes <78663710+Gabriel-Proenca@users.noreply.github.com>
2024-08-28 12:24:38 +0000

    resolução ex 01, porem a formatação de saida ainda não é boa

---
+++ Ex_01.py
@@ -0,0 +1,15 @@
+import math 
+
+entrada = input("Digite uma lista de 15 números, separados por vírgula: ")
+
+numeros = [float(x) for x in entrada.split(',')]
+
+for i in range(len(numeros)):
+    if numeros[i] < 0 :
+        numeros[i] = -1
+    else:
+        numeros[i] = math.sqrt(numeros[i])
+
+print("\nValores armazenados:")
+
+print(numeros)
